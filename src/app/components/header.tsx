cb46b90834fba5b173c091046e56bd4a549458cc
Vincent Wijaya <vwij0004@student.monash.edu>
2024-06-26 15:53:52 +0700

    Add navigation header

---
+++ src/app/components/header.tsx
@@ -0,0 +1,72 @@
+'use client';
+
+import React from 'react';
+import Image from 'next/image';
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+} from '@nextui-org/navbar';
+import { useState } from 'react';
+import { Link } from '@nextui-org/react';
+
+export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const menuItems = [
+    {
+      label: 'About Me',
+      id: 'about-me',
+    },
+    {
+      label: 'Experience',
+      id: 'experience',
+    },
+    {
+      label: 'Projects',
+      id: 'projects',
+    },
+    {
+      label: 'Education',
+      id: 'education',
+    },
+    {
+      label: 'Skills',
+      id: 'skills',
+    },
+    {
+      label: 'Contact',
+      id: 'contact',
+    }
+  ]
+
+  return (
+    <Navbar onMenuOpenChange={setIsMenuOpen}>
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+          className='sm:hidden'
+        />
+      </NavbarContent>
+
+      <NavbarContent className='hidden sm:flex gap-4' justify='center'>
+        {menuItems.map((item, index) => (
+          <NavbarItem key={`${item}-${index}`}>
+            <Link href={`#${item.id}`}>
+              {item.label}
+            </Link>
+          </NavbarItem>
+        ))}
+      </NavbarContent>
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>{item.label}</NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
+}
