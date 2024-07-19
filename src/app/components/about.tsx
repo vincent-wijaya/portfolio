74c8c22bec5afe93a1c5aa397d7fb307610f3490
Vincent Wijaya <vwij0004@student.monash.edu>
2024-07-19 11:23:01 +0700

    Replace ' with &apos;

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -5,7 +5,7 @@ export default function About() {
         <section id='about' className='flex flex-col flex-1 p-6 transition duration-300 hover:bg-opacity-10 hover:bg-white rounded-3xl w-full'>
             <h2 className='text-3xl font-bold'>About Me</h2>
             <p className='text-xl mt-4'>
-                Hello! I'm
+                Hello! I&apos;m
             </p>
             <p className='text-5xl mt-4 font-bold italic transition duration-300 hover:scale-125 text-center'>Vincent Wijaya</p>
             <p className='text-xl mt-4'>
