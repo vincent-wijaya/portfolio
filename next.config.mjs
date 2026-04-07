02bfaf5fc403ac56c5d5b9e8b62daeb022b55026
Vincent Wijaya <vincent@vwijaya.com>
2026-04-07 21:43:42 +1000

    Update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  output: 'export',
+  basePath: isProd ? '/portfolio' : '',
+  assetPrefix: isProd ? '/portfolio/' : '',
+  images: { unoptimized: true },
+};
 
 export default nextConfig;
